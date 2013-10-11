2c61ddfe7c19116fe7992e4f738271436ec8835a
Pedro Oliveira <pedro.hrde.oliveira@gmail.com>
2013-10-11 10:00:15 -0400

    added twitter scripts

--- index.php
+++ index.php
@@ -322,6 +322,7 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="/js/masonry.pkgd.min.js"></script>
 	<script src="/php/twitterAuth.php"></script>
+	<script src="/js/twitter-scripts.js"></script>
 	<script src="/js/post-scripts.js"></script>
 
 	</body>
