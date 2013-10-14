6235594ae0ca1e7683d2b02f8fbf4a77558eb46b
Pedro Oliveira <pedro.hrde.oliveira@gmail.com>
2013-10-14 12:36:26 -0400

    test

--- js/tweets.js
+++ js/tweets.js
@@ -1,23 +1,19 @@
 //Tweets
 
 
-function getTweets() {
+var url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+var data = "?count=2&screen_name=twitterapi";
 
-	var url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-	var data = "?count=2&screen_name=twitterapi";
-
-	$.ajax({
-		type: 'GET',
-		url: url + data,
-		success: function(data, textStatus, xhr) {
-			document.write(data, textStatus, xhr);
-		},
-
-		datatype: 'jsonp'
-
-		});
+$.ajax({
+	type: 'GET',
+	url: url + data,
+	datatype: 'jsonp'
+	jsonpCallback: 'getTweets'
 
 
+});
+
+function getTweets(r) {
+
+	console.log(r);
 }
-
-console.log(getTweets());
