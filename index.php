b20f97a3053d1e962911c857aa1e17ead76bbdb8
Pedro Oliveira <pedro.hrde.oliveira@gmail.com>
2014-03-27 15:18:05 -0400

    get tweets

--- index.php
+++ index.php
@@ -322,6 +322,7 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="/js/masonry.pkgd.min.js"></script>
 	<script src="/js/post-scripts.js"></script>
+	<script src="/php/get-tweets.php"></script>
 
 	</body>
 </html>
