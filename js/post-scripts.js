ec1def323af6bd94e48ff656f7369415c9eeb1ac
Pedro Oliveira <pedro.hrde.oliveira@gmail.com>
2014-03-27 15:32:08 -0400

    variable

--- js/post-scripts.js
+++ js/post-scripts.js
@@ -4,92 +4,101 @@
 
 var container = document.querySelector('.masonry');
 
-var msnry = new Masonry( container, {
-  // options...
-  itemSelector: '.item',
-  columnWidth: 280,
-  gutter: 4
+var msnry = new Masonry(container, {
+    // options...
+    itemSelector: '.item',
+    columnWidth: 280,
+    gutter: 4
 });
+
+var data = "http://playitbypixels.com/catalyst/php/get-tweets.php";
 
 $('.twitter-container').hide();
 
+
+
+function listTweets(data) {
+    console.log(data);
+}
+
 function twitterCall() {
 
- setTimeout(function(){
-
- 	
- 	var container = $('.twitter-container');
- 	container.show();
- 	var loader = $('.sm-twitter');
- 	var t = 0;
-	var frame = $('#twitter-widget-0');
-	var stream = frame.contents().find('.stream');;
-	var feed = stream.children().first();
-	var feedChildren = feed.children();
-	var tweet;
-	var tweetBody;
-	var tweetResults = [];
-
-	feedChildren.each(function(){
-		loader.children('.the-icons').hide();
-		tweet = $(this);
-
-		tweet.children('.header').children('.h-card').hide();
-		tweet.css({
-			'height' : '156px',
-			'position' : 'relative',
-			'box-sizing' : 'border-box'
-		});
-
-		tweet.children('.header').children().first().addClass('sm-date');
-
-		tweet.children('.footer').hide();
-
-		tweet.children('.e-entry-content').children().first().addClass('sm-text');
-		tweet.children('.e-entry-content').children('.retweet-credit').css({
-			'padding' : '10px 30px 0'
-		});
-
-		tweet.removeClass('tweet');
-
-		tweetResults.push(tweet);
-		
-	});
-
-	container.each(function(){	
-
-		t++;
-		$(this).children().replaceWith(tweetResults[t]);
-
-	});
+    setTimeout(function() {
 
 
-}, 700)
+        var container = $('.twitter-container');
+        container.show();
+        var loader = $('.sm-twitter');
+        var t = 0;
+        var frame = $('#twitter-widget-0');
+        var stream = frame.contents().find('.stream');;
+        var feed = stream.children().first();
+        var feedChildren = feed.children();
+        var tweet;
+        var tweetBody;
+        var tweetResults = [];
 
-clearTimeout();
+        feedChildren.each(function() {
+            loader.children('.the-icons').hide();
+            tweet = $(this);
+
+            tweet.children('.header').children('.h-card').hide();
+            tweet.css({
+                'height': '156px',
+                'position': 'relative',
+                'box-sizing': 'border-box'
+            });
+
+            tweet.children('.header').children().first().addClass('sm-date');
+
+            tweet.children('.footer').hide();
+
+            tweet.children('.e-entry-content').children().first().addClass('sm-text');
+            tweet.children('.e-entry-content').children('.retweet-credit').css({
+                'padding': '10px 30px 0'
+            });
+
+            tweet.removeClass('tweet');
+
+            tweetResults.push(tweet);
+
+        });
+
+        container.each(function() {
+
+            t++;
+            $(this).children().replaceWith(tweetResults[t]);
+
+        });
+
+
+    }, 700)
+
+    clearTimeout();
 
 }
 
 function vimeoLoad() {
-	var vimeo = document.getElementById('vimeo-container');
-	var vimeoFrame = document.createElement('iframe');
+    var vimeo = document.getElementById('vimeo-container');
+    var vimeoFrame = document.createElement('iframe');
 
-	vimeoFrame.src = '//player.vimeo.com/video/65696417';
-	vimeoFrame.width = 564;
-	vimeoFrame.height = 312;
-	vimeoFrame.frameborder = 0;
-	vimeoFrame.webkitallowfullscreen = true;
-	vimeoFrame.mozallowfullscreen = true;
-	vimeoFrame.allowfullscreen = true;
+    vimeoFrame.src = '//player.vimeo.com/video/65696417';
+    vimeoFrame.width = 564;
+    vimeoFrame.height = 312;
+    vimeoFrame.frameborder = 0;
+    vimeoFrame.webkitallowfullscreen = true;
+    vimeoFrame.mozallowfullscreen = true;
+    vimeoFrame.allowfullscreen = true;
 
-	// console.dir(vimeo);
-	vimeo.appendChild(vimeoFrame);
+    // console.dir(vimeo);
+    vimeo.appendChild(vimeoFrame);
 }
 
 
 function load() {
-	twitterCall();
-	vimeoLoad();
+    twitterCall();
+    vimeoLoad();
+    listTweets();
 }
 
 window.onload = load;
